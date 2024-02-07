097f3c7fc9a2ebf56c32e9f6841db5617d855690
Tuanemendes <tuane.belmiro@gmail.com>
2024-02-06 21:18:48 -0300

    feat: implementação do banco de dados

---
+++ todo-list-api/Data/TodoListContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using todo_list_api.Model;
+
+namespace todo_list_api.Data
+{
+    public class TodoListContext :DbContext
+    {
+        public TodoListContext(DbContextOptions<TodoListContext> options) : base(options){
+
+        }
+
+        public DbSet<TodoList> TodoLists{get; set;}
+         protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var todoList = modelBuilder.Entity<TodoList>();
+            todoList.ToTable("tb_todolist");
+            todoList.HasKey(x => x.Id);
+            todoList.Property(x => x.Id).HasColumnName("id_todolist").ValueGeneratedOnAdd();
+            todoList.Property(x => x.Description).HasColumnName("description");
+            todoList.Property(x => x.TodoStatus).HasColumnName("status");
+        }
+    }
+}
